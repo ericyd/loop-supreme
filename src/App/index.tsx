9b31b27e9442bd8c03a3d412de03b4822b7e2e7f
Eric Dauenhauer <eric@ericyd.com>
2022-11-04 12:12:59 -0500

    move header into static index

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,13 +5,7 @@ import { Metronome } from '../Metronome'
 function App() {
   return (
     <div className="sm:container mx-auto">
-      <h1 className="text-xl">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="font-serif">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Metronome />
-      </h1>
+      <Metronome />
     </div>
   )
 }
