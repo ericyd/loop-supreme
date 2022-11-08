ba804b06698c1b26be197d3a283ca1eda5da394b
Eric Dauenhauer <eric@ericyd.com>
2022-11-08 14:46:08 -0600

    add record button; subscribe to `beat` and `loopstart` events

--- src/Track/index.tsx
+++ src/Track/index.tsx
@@ -1,15 +1,52 @@
 import React, { ChangeEventHandler, useState } from 'react'
 
-type Props = {../Metronome
+type Props = {
   id: number
   onRemove(): void
 }
 
+const red = '#ef4444'
+const black = '#000000'
+
 export const Track: React.FC<Props> = (props) => {
+  const [metronome] = useMetronome()
   const [title, setTitle] = useState(`Track ${props.id}`)
+  const [armed, setArmed] = useState(false)
+  const [recording, setRecording] = useState(false)
+  const [audioData, setAudioData] = useState<Blob[]>([])
+  // TODO: should I be using a tailwind class for this?
+  const [recordButtonColor, setRecordButtonColor] = useState(
+    recording ? red : black
+  )
+
+  const audioRouter = useAudioRouter()
+
   const handleChangeTitle: ChangeEventHandler<HTMLInputElement> = (event) => {
     setTitle(event.target.value)
   }
+
+  // if track is armed, toggle the color between red and black every half-beat
+  function handleBeat() {
+    if (armed) {
+      setRecordButtonColor(red)
+      setTimeout(() => {
+        setRecordButtonColor(black)
+      }, (60 / metronome.bpm / 2) * 1000)
+    }
+  }
+
+  function handleLoopstart() {
+    // TODO
+  }
+
+  useEffect(() => {
+    metronome.events.addEventListener('beat', handleBeat)
+    metronome.events.addEventListener('loopstart', handleLoopstart)
+    return () => {
+      metronome.events.removeEventListener('beat', handleBeat)
+      metronome.events.removeEventListener('loopstart', handleLoopstart)
+    }
+  })
   return (
     <div>
       <input value={title} onChange={handleChangeTitle} />
