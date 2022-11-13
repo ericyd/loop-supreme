87e759aa1a25ff3afc117ae4dea793c7ed4de121
Eric Yancey Dauenhauer <eric@ericyd.com>
2022-11-13 12:52:13 -0600

    Improve control panel styling (#10)

--- src/ControlPanel/index.tsx
+++ src/ControlPanel/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDebouncedCallback } from 'use-debounce'
 import { MetronomeReader, MetronomeWriter } from '../Metronome'
 import play from '../icons/iconmonstr-media-control-48.svg'
 import pause from '../icons/iconmonstr-media-control-49.svg'
-import { Container } from '../Container'
 
 type Props = {
   metronome: MetronomeReader
@@ -14,20 +13,25 @@ export const ControlPanel: React.FC<Props> = ({
   metronome,
   metronomeWriter,
 }) => {
-  const handleSetBpm: React.ChangeEventHandler<HTMLInputElement> =
-    useDebouncedCallback(
-      (event) => {
-        const bpm = Number(event.target.value)
-        if (Number.isNaN(bpm)) {
-          throw new Error(
-            `Could not convert bpm "${event.target.value}" to numeric`
-          )
-        }
-        metronomeWriter.setBpm(bpm)
-      },
-      100,
-      { leading: true, trailing: false }
-    )
+  const [visualBpm, setVisualBpm] = useState(120)
+  const setUpstreamBpm = useDebouncedCallback(
+    (bpm: number) => {
+      metronomeWriter.setBpm(bpm)
+    },
+    100,
+    { leading: true, trailing: false }
+  )
+  // TODO: something about this isn't working right
+  const handleSetBpm: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const bpm = Number(event.target.value)
+    if (Number.isNaN(bpm)) {
+      throw new Error(
+        `Could not convert bpm "${event.target.value}" to numeric`
+      )
+    }
+    setUpstreamBpm(bpm)
+    setVisualBpm(bpm)
+  }
 
   const handleSetTimeSignature: React.ChangeEventHandler<HTMLSelectElement> = (
     event
@@ -58,19 +62,25 @@ export const ControlPanel: React.FC<Props> = ({
   }
 
   return (
-    <Container title="Control panel">
-      <div className="flex items-start content-center mb-2">
-        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-initial mr-2">
-          <div>tick: {metronome.currentTick}</div>
+    <div className="flex flex-col">
+      <div className="flex items-stretch justify-between content-center mb-2">
+        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-grow mr-2">
+          <span className="font-serif text-4xl pr-3">
+            {/* `+ 1` to convert "computer numbers" to "musician numbers"  */}
+            {metronome.currentTick + 1}
+          </span>
+          <span className="font-serif text-xl">
+            / {metronome.currentMeasure + 1}
+          </span>
         </div>
 
-        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-initial mr-2">
-          <div>measure: {metronome.currentMeasure}</div>
-        </div>
-
-        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-initial mr-2">
-          {/* TODO: read directly from the input[type="range"] below. This reads the debounced value which is confusing from a UX perspective */}
-          <div>bpm: {metronome.bpm}</div>
+        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-grow mr-2">
+          <div>
+            <span className="font-serif text-4xl pr-3">
+              {visualBpm.toFixed(1)}
+            </span>
+            <span className="font-serif text-xl">BPM</span>
+          </div>
           <input
             type="range"
             onChange={handleSetBpm}
@@ -81,19 +91,26 @@ export const ControlPanel: React.FC<Props> = ({
           />
         </div>
 
-        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-initial mr-2">
-          <div>time signature</div>
+        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-grow mr-2">
           <select
             onChange={handleSetTimeSignature}
             value={`${metronome.timeSignature.beatsPerMeasure}/${metronome.timeSignature.beatUnit}`}
+            className="p-2 font-serif text-4xl bg-white"
           >
             <option value="4/4">4/4</option>
             <option value="7/8">7/8</option>
           </select>
         </div>
 
-        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-initial mr-2">
-          <div>measure count: {metronome.measureCount}</div>
+        <div className="p-2 border border-zinc-400 border-solid rounded-sm flex-grow mr-2">
+          <div>
+            <span className="font-serif text-4xl pr-3">
+              {metronome.measureCount}
+            </span>
+            <span className="font-serif text-xl">
+              measure{metronome.measureCount === 1 ? '' : 's'}
+            </span>
+          </div>
           <input
             type="range"
             min="1"
@@ -103,7 +120,9 @@ export const ControlPanel: React.FC<Props> = ({
             onChange={handleChangeMeasureCount}
           />
         </div>
+      </div>
 
+      <div className="flex items-start content-center mb-2">
         <button
           onClick={metronomeWriter.togglePlaying}
           className="p-2 border border-zinc-400 border-solid rounded-sm flex-initial mr-2"
@@ -114,6 +133,6 @@ export const ControlPanel: React.FC<Props> = ({
           />
         </button>
       </div>
-    </Container>
+    </div>
   )
 }
