5380b3cc469fe6e1c4f74502e62f25d4d70b4fef
Eric Dauenhauer <eric@ericyd.com>
2022-11-13 11:54:20 -0600

    add recorder AudioWorkletProcessor

---
+++ src/worklets/recorder.js
@@ -0,0 +1,139 @@
+// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// copied wholesale from https://github.com/GoogleChromeLabs/web-audio-samples/blob/eed2a8613af551f2b1d166a01c834e8431fdf3c6/src/audio-worklet/migration/worklet-recorder/recording-processor.js
+
+/**
+ * Why JS?
+ *    This is hopefully temporary, but I'm not sure all the right TS declarations I need to make for this to compile as TS
+ * The code in this file was heavily inspired by this Google example, and Monica Dinculescu's fantastic metronome test/example code:
+ *    https://github.com/GoogleChromeLabs/web-audio-samples/blob/eed2a8613af551f2b1d166a01c834e8431fdf3c6/src/audio-worklet/migration/worklet-recorder/recording-processor.js
+ *    https://glitch.com/edit/#!/metronomes?path=worker.js%3A1%3A0
+ */
+class RecordingProcessor extends AudioWorkletProcessor {
+  constructor(options) {
+    super()
+
+    this.hasPostedInputs = false
+    this.sampleRate = 0
+    this.maxRecordingFrames = 0
+    this.numberOfChannels = 0
+
+    if (options && options.processorOptions) {
+      const { numberOfChannels, sampleRate, maxFrameCount } =
+        options.processorOptions
+
+      this.sampleRate = sampleRate
+      this.maxRecordingFrames = maxFrameCount
+      this.numberOfChannels = numberOfChannels
+    }
+
+    this._recordingBuffer = new Array(this.numberOfChannels).fill(
+      new Float32Array(this.maxRecordingFrames)
+    )
+
+    this.recordedFrames = 0
+    this.isRecording = false
+
+    // We will use a timer to gate our messages; this one will publish at 60hz
+    this.framesSinceLastPublish = 0
+    this.publishInterval = this.sampleRate / 60
+
+    // We will keep a live sum for rendering the visualizer.
+    this.sampleSum = 0
+
+    this.port.onmessage = (event) => {
+      if (event.data.message === 'UPDATE_RECORDING_STATE') {
+        this.isRecording = event.data.setRecording
+
+        if (this.isRecording === false) {
+          this.port.postMessage({
+            message: 'SHARE_RECORDING_BUFFER',
+            buffer: this._recordingBuffer,
+          })
+        }
+      }
+    }
+  }
+
+  process(inputs, outputs, params) {
+    // I *think* this is required because of a short delay between when the AudioWorkletProcessor is registered, and when the source stream is connected to it
+    if (inputs.length === 0 || inputs[0].length === 0) {
+      return
+    }
+    // this is totally unnecessary, just wanted it for some debugging
+    if (!this.hasPostedInputs) {
+      console.log({ inputs })
+      this.port.postMessage({
+        message: JSON.stringify(inputs),
+      })
+      this.hasPostedInputs = true
+    }
+    this.port.postMessage({
+      message: `${inputs.length} inputs; ${inputs[0]} channels`,
+    })
+    for (let input = 0; input < inputs.length; input++) {
+      for (let channel = 0; channel < inputs[input].length; channel++) {
+        for (let sample = 0; sample < inputs[input][channel].length; sample++) {
+          const currentSample = inputs[input][channel][sample]
+
+          // Copy data to recording buffer.
+          if (this.isRecording) {
+            this._recordingBuffer[channel][sample + this.recordedFrames] =
+              currentSample
+          }
+
+          // Pass data directly to output, unchanged.
+          outputs[input][channel][sample] = currentSample
+
+          // Sum values for visualizer
+          this.sampleSum += currentSample
+        }
+      }
+    }
+
+    const shouldPublish = this.framesSinceLastPublish >= this.publishInterval
+
+    // Validate that recording hasn't reached its limit.
+    if (this.isRecording) {
+      if (this.recordedFrames + 128 < this.maxRecordingFrames) {
+        this.recordedFrames += 128
+
+        // Post a recording recording length update on the clock's schedule
+        if (shouldPublish) {
+          this.port.postMessage({
+            message: 'UPDATE_RECORDING_LENGTH',
+            recordingLength: this.recordedFrames,
+          })
+        }
+      } else {
+        // Let the rest of the app know the limit was reached.
+        this.isRecording = false
+        this.port.postMessage({
+          message: 'MAX_RECORDING_LENGTH_REACHED',
+        })
+
+        return false
+      }
+    }
+
+    // Handle message clock.
+    // If we should publish, post message and reset clock.
+    if (shouldPublish) {
+      this.port.postMessage({
+        message: 'UPDATE_VISUALIZERS',
+        gain: this.sampleSum / this.framesSinceLastPublish,
+      })
+
+      this.framesSinceLastPublish = 0
+      this.sampleSum = 0
+    } else {
+      this.framesSinceLastPublish += 128
+    }
+
+    return true
+  }
+}
+
+registerProcessor('recorder', RecordingProcessor)
