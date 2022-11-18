e848539f17aae9f8e0b705ae834b2509690a9271
Eric Yancey Dauenhauer <eric@ericyd.com>
2022-11-17 21:06:48 -0600

    Index.html updates (#16)
    * small refactor for readability
    
    * basic SEO / OG tags
    
    * improve Start page styling
    
    * remove useInterval hook
    
    * update roadmap
    
    * refactor LatencyNotSupportedAlert

--- src/Start/index.tsx
+++ src/Start/index.tsx
@@ -15,13 +15,23 @@
  * useful in the first place.
  * Hence, passing as props (after the user affirmatively consents) felt like the simplest solution.
  */
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import App from '../App'
 import { logger } from '../util/logger'
 
 export const Start: React.FC = () => {
   const [stream, setStream] = useState<MediaStream>()
   const [audioContext, setAudioContext] = useState<AudioContext>()
+  const [latencySupported, setLatencySupported] = useState(true)
+
+  useEffect(() => {
+    const supportedConstraints =
+      navigator.mediaDevices.getSupportedConstraints()
+    // @ts-expect-error This is a documented property, and indeed it is `true` in Chrome https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackSupportedConstraints/latency
+    if (!supportedConstraints.latency) {
+      setLatencySupported(false)
+    }
+  }, [])
 
   async function handleClick() {
     try {
@@ -53,6 +63,40 @@ export const Start: React.FC = () => {
   return stream && audioContext ? (
     <App stream={stream} audioContext={audioContext} />
   ) : (
-    <button onClick={handleClick}>Start</button>
+    <>
+      <div className="flex flex-col items-center justify-center mx-auto">
+        {!latencySupported && <LatencyNotSupportedAlert />}
+        <button
+          onClick={handleClick}
+          className="px-10 py-5 border-4 border-solid border-black rounded-sm text-xl font-serif font-bold bg-blue-200"
+        >
+          Start
+        </button>
+      </div>
+    </>
+  )
+}
+
+function LatencyNotSupportedAlert() {
+  return (
+    <div className="mb-5 font-bold bg-red-200 p-2 rounded-md">
+      <p>Heads up!</p>
+      <p>Your browser does not appear to report recording latency 😢.</p>
+      <p>
+        You are free to continue, but your loops will most likely fall behind
+        the beat.
+      </p>
+      <p>For best results, try Chrome (we know, we tried, we're sorry)</p>
+      <p>
+        <a
+          href="https://ericyd.hashnode.dev/loop-supreme-part-7-latency-and-adding-track-functionality"
+          target="_blank"
+          rel="noreferrer"
+          className="underline text-cyan-600"
+        >
+          (why tho?)
+        </a>
+      </p>
+    </div>
   )
 }
--- src/Track/get-latency-samples.ts
+++ src/Track/get-latency-samples.ts
@@ -12,12 +12,10 @@ export function getLatencySamples(
   stream: MediaStream,
   audioContext: AudioContext
 ): number {
+  // This check is duplicated in Start/index.tsx but it is fine to re-check here
   const supportedConstraints = navigator.mediaDevices.getSupportedConstraints()
   // @ts-expect-error This is a documented property, and indeed it is `true` in Chrome https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackSupportedConstraints/latency
   if (!supportedConstraints.latency) {
-    // TODO: should we make a guess about recording latency?
-    // Even in browsers where this is not reported, it is clear that latency is non-zero.
-    // In very simple tests, it is often > 100ms
     logger.log({
       message:
         'Could not get track latency because this environment does not report latency on MediaStreamTracks',
@@ -25,66 +23,64 @@ export function getLatencySamples(
     })
     return 0
   }
-  const recordingStreamLatency = stream
-    .getAudioTracks()
-    .reduce((maxChannelLatency, channel, i) => {
-      // In Chrome, this is the best (only?) way to get the track latency
-      // Strangely, it isn't even documented on MDN https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/getCapabilities
-      const capabilitiesLatency =
-        typeof channel.getCapabilities === 'function' &&
-        channel.getCapabilities?.()?.latency?.max
-      if (typeof capabilitiesLatency === 'number') {
-        return Math.max(maxChannelLatency, capabilitiesLatency)
-      }
-      logger.debug({
-        message: 'Could not get capabilities, or latency was not a number',
-        channel: i,
-        typeofGetCapabilities: typeof channel.getCapabilities,
-        capabilitiesLatency,
-      })
+  const recordingStreamLatencies = stream.getAudioTracks().map((channel, i) => {
+    // In Chrome, this is the best (only?) way to get the track latency
+    // Strangely, it isn't even documented on MDN https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/getCapabilities
+    const capabilitiesLatency =
+      typeof channel.getCapabilities === 'function' &&
+      channel.getCapabilities?.()?.latency?.max
+    if (typeof capabilitiesLatency === 'number') {
+      return capabilitiesLatency
+    }
+    logger.debug({
+      message: 'Could not get capabilities, or latency was not a number',
+      channel: i,
+      typeofGetCapabilities: typeof channel.getCapabilities,
+      capabilitiesLatency,
+    })
 
-      // this should be an alternative, but doesn't appear to be populated in Firefox https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints/latency
-      const constraintLatency =
-        typeof channel.getConstraints === 'function' &&
-        channel.getConstraints?.()?.latency
-      if (typeof constraintLatency === 'number') {
-        return Math.max(maxChannelLatency, constraintLatency)
-      }
-      logger.debug({
-        message: 'Could not get constraints, or latency was not a number',
-        channel: i,
-        typeofGetConstraints: typeof channel.getConstraints,
-        constraintLatency,
-      })
+    // this should be an alternative, but doesn't appear to be populated in Firefox https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints/latency
+    const constraintLatency =
+      typeof channel.getConstraints === 'function' &&
+      channel.getConstraints?.()?.latency
+    if (typeof constraintLatency === 'number') {
+      return constraintLatency
+    }
+    logger.debug({
+      message: 'Could not get constraints, or latency was not a number',
+      channel: i,
+      typeofGetConstraints: typeof channel.getConstraints,
+      constraintLatency,
+    })
 
-      // this is yet another alternative according to MDN but not implemented in major browsers yet https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackSettings/latency
-      // Just leaving as a fall back in case browsers implement in the future
-      const settingsLatency =
-        typeof channel.getSettings === 'function' &&
-        // @ts-expect-error lib.dom.d.ts doesn't believe this is a valid option (which I suppose is technically true)
-        channel.getSettings?.()?.latency
-      if (typeof settingsLatency === 'number') {
-        return Math.max(maxChannelLatency, settingsLatency)
-      }
-      logger.debug({
-        message: 'Could not get settings, or latency was not a number',
-        channel: i,
-        typeofGetSettings: typeof channel.getSettings,
-        settingsLatency,
-      })
+    // this is yet another alternative according to MDN but not implemented in major browsers yet https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackSettings/latency
+    // Just leaving as a fall back in case browsers implement in the future
+    const settingsLatency =
+      typeof channel.getSettings === 'function' &&
+      // @ts-expect-error lib.dom.d.ts doesn't believe this is a valid option (which I suppose is technically true)
+      channel.getSettings?.()?.latency
+    if (typeof settingsLatency === 'number') {
+      return settingsLatency
+    }
+    logger.debug({
+      message: 'Could not get settings, or latency was not a number',
+      channel: i,
+      typeofGetSettings: typeof channel.getSettings,
+      settingsLatency,
+    })
 
-      return maxChannelLatency
-    }, 0)
+    return 0
+  })
 
   const latencySeconds = Math.max(
     audioContext.baseLatency,
     audioContext.outputLatency,
-    recordingStreamLatency
+    ...recordingStreamLatencies
   )
   logger.debug({
     'audioContext.baseLatency': audioContext.baseLatency,
     'audioContext.outputLatency': audioContext.outputLatency,
-    recordingStreamLatency,
+    recordingStreamLatencies,
     latencySeconds,
   })
   return Math.ceil(latencySeconds * sampleRate)
